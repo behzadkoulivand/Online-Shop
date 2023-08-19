a5d1cbc1b4d57fd6c37e5f5af48a8db393d363ed
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-19 21:50:43 +0330

    Set middlewares in app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const dotEnv = require("dotenv");
 
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middlewares/errors");
+const { setHeaders } = require("./middlewares/setHeaders");
+
+// Config Folder
+dotEnv.config({ path: "./config/config.env" });
 
 // MongoDB Connection
 connectDB();
@@ -10,9 +16,13 @@ const app = express();
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(setHeaders);
 
 // Routes
 app.use("/users", require("./routes/user"));
+
+// Error Handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
