eb9e01317a86c28466e01b5db2e4dd9994bd8238
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-19 21:31:22 +0330

    middlewares added to app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
+// MongoDB Connection
+connectDB();
+
 const app = express();
+
+// Body Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use("/users", require("./routes/user"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
